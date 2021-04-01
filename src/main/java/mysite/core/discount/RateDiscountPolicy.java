3bf10b6c8e58afd1bdd3e07566d24c5ebecfcc3c
Kim Taekyung <seaking7@gmail.com>
2021-04-01 19:42:13 +0900

    Annotation추가

--- src/main/java/mysite/core/discount/RateDiscountPolicy.java
+++ src/main/java/mysite/core/discount/RateDiscountPolicy.java
@@ -1,8 +1,12 @@
 package mysite.core.discount;
 
+import mysite.core.annotation.MainDiscountPolicy;
 import mysite.core.member.Grade;
 import mysite.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
