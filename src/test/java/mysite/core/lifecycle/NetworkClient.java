90493fa5fd5f4ab786b0efebac213e22d0f0b547
Kim Taekyung <seaking7@gmail.com>
2021-04-03 11:19:22 +0900

    스프링빈 초기화,소멸이벤트

---
+++ src/test/java/mysite/core/lifecycle/NetworkClient.java
@@ -0,0 +1,42 @@
+package mysite.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url : "+url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    
+    public void connect(){
+        System.out.println("NetworkClient.connect:"+url);
+    }
+
+    public void call(String message){
+        System.out.println("NetworkClient.call url: "+ url + " message= "+message);
+    }
+
+    public void disconnect(){
+        System.out.println("NetworkClient.disconnect url "+url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+    }
+}
